b8ff9f35268033e2fce03f648ec956c843ddda09
Kevin Dang <kevinthedang_1@outlook.com>
2022-03-24 13:49:43 -0700

    notes taken for room

--- room_chat_api.py
+++ room_chat_api.py
@@ -9,7 +9,6 @@ from constants import *
 from users import *
 
 MY_IPADDRESS = ""
-USER_GENERAL_LIST = 'general_users'
 
 # This is an extremely rare case where I have global variables. The first is the documented way to deal with running the app in uvicorn, the second is the 
 # instance of the rmq class that is necessary across all handlers that behave essentially as callbacks. 
@@ -25,72 +24,62 @@ logging.basicConfig(filename='chat.log', level=logging.INFO)
 async def index():
     """ Default page
     """
-    return {"message": {"from": "kevin", "to": "you"}}
+    pass
+
+@app.get("/page/send", status_code=200)
+async def send_form(request: Request):
+    """ HTML GET page form sending a message
+    """
+    pass
+
+@app.post("/page/send", status_code=201)
+async def get_form(request: Request, room_choice: str = Form(...), message: str = Form(...), alias: str = Form(...)):
+    """ HTML POST page for sending a message
+    """
+    pass
+
+@app.get("/page/messages", status_code=200)
+async def form_messages(request: Request, room_name: str = DEFAULT_PUBLIC_ROOM):
+    """ HTML GET page for seeing messages
+    """
+    pass
+
+@app.post("/page/messages", status_code=201)
+async def form_messages(request: Request, room_name: str = Form(...)):
+    """ HTML POST page for seeing messages in a different room or different quantities
+    """
+    pass
+
 
 @app.get("/messages/", status_code=200)
 async def get_messages(request: Request, alias: str, room_name: str, messages_to_get: int = GET_ALL_MESSAGES):
     """ API for getting messages
-        NOTE: Refer to get_messages() in the room.py file
     """
-    logging.info("starting messages method")
-    if (queue_instance := ChatRoom(room_name = room_name, member_list = users, owner_alias = DEFAULT_OWNER_ALIAS)) is None:
-        return JSONResponse(status_code=415, content=f'ChatRoom does not exist.')
-    messages, message_objects, total_mess = queue_instance.get_messages(num_messages = messages_to_get)
-    logging.info(f'inside messages handler, after getting messages for queue: {alias}\n messages are {messages}')
-    for message in message_objects:
-        logging.info(f'Message: {message.message} == message props: {message.mess_props} host is {request.client.host}')
-        logging.info(request.json())
-    logging.info("End Messages")
-    return messages
+    pass
 
 @app.get("/users/", status_code=200)
 async def get_users():
     """ API for getting users
-        NOTE: Might want to consider using get_all_users() from the users.py
     """
-    try:
-        users = UserList()
-    except:
-        users = UserList(USER_GENERAL_LIST)
-    if users.get_all_users() is not None:
-        return "success"
-    else:
-        return Response(status_code=410, content="user_list was empty")
+    pass
 
 @app.post("/alias", status_code=201)
-async def register_client(client_alias: str):
+async def register_client(client_alias: str, group_alias: bool = False):
     """ API for adding a user alias
-        NOTE: Refer to the users.py on what might be called here
-        NOTE: Code to register a user/alias. Simple method calls
     """
-    try:
-        users = UserList()
-    except:
-        users = UserList(USER_GENERAL_LIST)
-    if users.get(client_alias) is None:
-        users.register(client_alias)
-        return "success"
-    else:
-        return Response(status_code=410, content="User exists already")
+    pass
 
 @app.post("/room")
 async def create_room(room_name: str, owner_alias: str, room_type: int = ROOM_TYPE_PRIVATE):
     """ API for creating a room
-        NOTE: Refer to the room.py -> RoomList -> Create() or add() to know what the API should do
-            add() maybe for adding the room to the MongoDB
     """
-    room_list.add(room_list.create(room_name = room_name, owner_alias = owner_alias, room_type = room_type))
+    pass
 
 @app.post("/message/", status_code=201)
-async def send_message(room_name: str, message: str):
+async def send_message(room_name: str, message: str, from_alias: str, to_alias: str):
     """ API for sending a message
-        NOTE: Refer to send_message() in room.py, may help
     """
-    current_room = room_list.get(room_name)
-    if current_room.send_message(message = message) is True:
-        return 'success'
-    else:
-        return JSONResponse(status_code=410, content="unroutable error")
+    pass
 
 def main():
     logging.basicConfig(filename='chat.log', level=logging.INFO)
